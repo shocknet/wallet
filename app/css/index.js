6e22e605a59b8033c4906c2c2c83cd3e2724ced8
Daniel Lugo <danlugo92@gmail.com>
2019-11-27 18:18:14 -0400

    new colors

--- app/css/index.js
+++ app/css/index.js
@@ -2,13 +2,13 @@
  * @prettier
  */
 export const Colors = {
-  BLUE_DARK: '#2A5298',
+  BLUE_DARK: '#2E4674',
   BLUE_LIGHTEST: '#F4F6FA',
   BLUE_LIGHT: '#267ADB',
   BLUE_GRAY: '#50668F',
   GRAY_DARK: '#4E4E4E',
-  GRAY_MEDIUM: '#F5F5F5',
-  ORANGE: '#F5A623',
+  GRAY_MEDIUM: '#505050',
+  ORANGE: '#F8A61E',
   TEAL: '#4285B9',
   TEXT_LIGHTEST: '#979797',
   TEXT_LIGHT: '#787878',
