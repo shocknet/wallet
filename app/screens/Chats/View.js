1789457f85d5c7ec9769bc17a9de715d2c691117
emad-salah <emads14@gmail.com>
2020-01-06 16:47:02 +0100

    Fixed messages screen scrolling

--- app/screens/Chats/View.js
+++ app/screens/Chats/View.js
@@ -381,7 +381,7 @@ export default class ChatsView extends React.PureComponent {
           data={items}
           keyExtractor={keyExtractor}
           renderItem={this.itemRenderer}
-          contentContainerStyle={LIST_STYLE}
+          style={LIST_STYLE}
         />
 
         <ShockDialog
