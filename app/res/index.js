671392eaedeed8ec3ba3911b46f53a15eb14c5a6
Daniel Lugo <danlugo92@gmail.com>
2020-01-20 18:41:11 -0400

    new asset

--- app/res/index.js
+++ app/res/index.js
@@ -9,3 +9,7 @@ export const shockLogo = require('../assets/images/shocklogo.png')
 /** @type {number} */
 // @ts-ignore
 export const shockBG2 = require('../assets/images/shock-bg-2.png')
+
+/** @type {number} */
+// @ts-ignore
+export const lock = require('../assets/images/lock.png')
