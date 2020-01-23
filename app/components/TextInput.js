c1c7b4d755af0415a05eed4c48db3b3b632351ff
Daniel Lugo <danlugo92@gmail.com>
2020-01-22 22:48:00 -0400

    TextInput with no padding

---
+++ app/components/TextInput.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react'
+import { TextInput as TextInputRN } from 'react-native'
+
+/**
+ * A TextInput with no padding.
+ * @type {React.FC<import('react-native').TextInputProps>}
+ */
+const TextInput = React.memo(
+  React.forwardRef((props, ref) => (
+    <TextInputRN
+      // @ts-ignore
+      paddingTop={0}
+      paddingBottom={0}
+      paddingLeft={0}
+      paddingRight={0}
+      ref={ref}
+      {...props}
+    />
+  )),
+)
+
+export default TextInput
