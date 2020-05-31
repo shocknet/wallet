e97ab1b9d92cbdbf7195c5afade75ee7ea6bc7eb
Daniel Lugo <danlugo92@gmail.com>
2020-05-30 20:22:51 -0400

    follow btn

---
+++ app/components/FollowBtn.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import { Button } from 'react-native-elements'
+import { connect } from 'react-redux'
+import * as Common from 'shock-common'
+import * as Store from '../../store'
+import * as Actions from '../actions'
+
+/**
+ * @typedef {object} StateProps
+ * @prop {Common.Schema.Follow | null} follow
+ */
+
+/**
+ * @typedef {object} DispatchProps
+ * @prop {() => void} onPressFollow
+ * @prop {() => void} onPressUnfollow
+ */
+
+/**
+ * @typedef {object} OwnProps
+ * @prop {string} publicKey
+ */
+
+/**
+ * @type {React.FC<StateProps & DispatchProps & OwnProps>}
+ */
+export const FollowBtn = ({ follow, onPressFollow, onPressUnfollow }) => {
+  /**
+   * @type {Common.Schema.Follow['status'] | 'unfollowed'}
+   */
+  const state = follow ? follow.status : 'unfollowed'
+
+  const title = (() => {
+    switch (state) {
+      case 'ok':
+        return 'unfollow'
+      case 'processing':
+        return '...'
+      case 'unfollowed':
+        return 'follow'
+    }
+  })()
+
+  return ((
+    <Button
+      // eslint-disable-next-line react/jsx-no-bind
+      onPress={() => {
+        switch (state) {
+          case 'ok':
+            onPressUnfollow()
+            break
+          case 'unfollowed':
+            onPressFollow()
+            break
+        }
+      }}
+      title={title}
+      disabled={state === 'processing'}
+    />
+  ))
+}
+
+/**
+ * @param {Store.State} state
+ * @param {OwnProps} ownProps
+ * @returns {StateProps}
+ */
+const mapState = (state, ownProps) => ({
+  follow: Store.Selectors.Follows.getFollow(state, ownProps.publicKey),
+})
+
+//@type {import('react-redux').MapDispatchToProps<DispatchProps, OwnProps>}
+/**
+ * @param {import('redux').Dispatch<Actions.Action>} dispatch
+ * @param {OwnProps} ownProps
+ * @returns {DispatchProps}
+ */
+const mapDispatch = (dispatch, ownProps) => ({
+  onPressFollow: () => {
+    dispatch(Actions.Follows.beganFollow(ownProps.publicKey))
+  },
+  onPressUnfollow: () => {
+    dispatch(Actions.Follows.beganUnfollow(ownProps.publicKey))
+  },
+})
+
+export default connect(
+  mapState,
+  mapDispatch,
+)(FollowBtn)
