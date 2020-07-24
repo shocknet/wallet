2cc0a64eef871e1dab41fce2067e75dd96fa3643
hatim boufnichel <boufni95@gmail.com>
2020-07-24 21:14:30 +0200

    add check for sockets

--- app/services/contact-api/socket.js
+++ app/services/contact-api/socket.js
@@ -6,7 +6,8 @@ import isEmpty from 'lodash/isEmpty'
 import debounce from 'lodash/debounce'
 import Logger from 'react-native-file-log'
 import { Constants } from 'shock-common'
-
+//@ts-ignore
+import { DISABLE_SHOCK_ENCRYPTION } from 'react-native-dotenv'
 import * as Cache from '../../services/cache'
 import { ACTIONS as ConnectionAction } from '../../actions/ConnectionActions'
 
@@ -68,6 +69,10 @@ export const setStore = initializedStore => {
  * @param {object} data
  */
 export const encryptSocketData = async data => {
+  if (DISABLE_SHOCK_ENCRYPTION === 'true') {
+    return data
+  }
+
   const { APIPublicKey } = store.getState().connection
 
   Logger.log('APIPublicKey', APIPublicKey)
