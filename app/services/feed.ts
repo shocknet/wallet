a5f862445d077e81a53e0f2022f4e41896a36193
Daniel Lugo <danlugo92@gmail.com>
2020-08-14 23:48:10 -0400

    new feed page fetcher

--- app/services/feed.ts
+++ app/services/feed.ts
@@ -1,7 +1,27 @@
 import Http, { AxiosResponse } from 'axios'
+import { feedPage as commonFeedPage } from 'shock-common/src/store/api'
 
 export const getFeedPage = async (
   pageNumber: number,
 ): Promise<AxiosResponse> => {
   return await Http.get(`/api/gun/feed?page=${pageNumber}`)
+}
+
+export const feedPage: typeof commonFeedPage = async ({ before, page }) => {
+  if (page) {
+    const res = await Http.get(`api/gun/feed?page=${page}`)
+
+    console.log('xcx')
+    console.log(res.data)
+
+    return res.data
+  }
+
+  if (before) {
+    const res = await Http.get(`api/gun/feed?before=${before}`)
+
+    return res.data
+  }
+
+  throw new ReferenceError('Must provide either page or before param')
 }
