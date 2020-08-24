ca4508f6a973dbdc3804d1ff933e35b3cf74ff13
Daniel Lugo <danlugo92@gmail.com>
2020-08-23 20:05:39 -0400

    complete thunk

--- app/thunks/tips.ts
+++ app/thunks/tips.ts
@@ -2,14 +2,24 @@ import { ToastAndroid } from 'react-native'
 import { Dispatch } from 'redux'
 import Logger from 'react-native-file-log'
 
+import { State } from '../../reducers'
 import * as Wallet from '../services/wallet'
 import { Action, requestedTip, tipWentThrough, tipFailed } from '../actions'
 
-export const tip = async (
+export const tip = (
   amount: number,
   recipientsPublicKey: string,
   memo: string,
-) => (dispatch: Dispatch<Action>) => {
+) => (dispatch: Dispatch<Action>, getState: () => State) => {
+  const { tips } = getState()
+
+  const tipOrUndef = tips[recipientsPublicKey]
+
+  // only one tip process at a time
+  if (tipOrUndef && tipOrUndef.state === 'processing') {
+    return
+  }
+
   dispatch(requestedTip(amount, recipientsPublicKey, memo))
 
   Wallet.tip(amount, recipientsPublicKey, memo, 0)
