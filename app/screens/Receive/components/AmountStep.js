52db7f5a02e0be392612c8cfe58cc90bcdabe4e7
Daniel Lugo <danlugo92@gmail.com>
2020-08-29 18:15:10 -0400

    dark <AmountStep />

--- app/screens/Receive/components/AmountStep.js
+++ app/screens/Receive/components/AmountStep.js
@@ -24,6 +24,7 @@ import {
  * @prop {(amount:string)=>void} setAmount
  * @prop {(description:string)=>void} setDescription
  * @prop {(unit:import('../../../actions/InvoiceActions').SelectedUnit)=>void} setUnitSelected
+ * @prop {(string)=} theme
  */
 /**
  * @typedef {ConnectedRedux & TmpProps} Props
@@ -37,13 +38,16 @@ const AmountStep = ({
   setDescription,
   setUnitSelected,
   invoice,
+  theme = 'dark',
 }) => ((
   <>
-    <Text style={styles.stepTitle}>Enter Amount</Text>
+    {theme === 'dark' ? null : (
+      <Text style={styles.stepTitle}>Enter Amount</Text>
+    )}
     <View>
       <View style={styles.amountContainer}>
         <InputGroup
-          label="Amount"
+          label={theme === 'dark' ? 'Enter Amount' : 'Amount'}
           value={invoice.amount}
           onChange={setAmount}
           style={styles.amountInput}
@@ -59,14 +63,32 @@ const AmountStep = ({
             },
           ]}
           onChangeText={setUnitSelected}
-          containerStyle={styles.amountSelect}
+          containerStyle={
+            theme === 'dark' ? styles.amountSelectDark : styles.amountSelect
+          }
           value={invoice.unitSelected ? 'sats' : invoice.unitSelected}
           lineWidth={0}
-          inputContainerStyle={styles.amountSelectInput}
+          inputContainerStyle={
+            theme === 'dark'
+              ? styles.amountSelectInputDark
+              : styles.amountSelectInput
+          }
           rippleOpacity={0}
-          pickerStyle={styles.amountPicker}
-          dropdownOffset={{ top: 8, left: 0 }}
-          rippleInsets={{ top: 8, bottom: 0, right: 0, left: 0 }}
+          pickerStyle={
+            theme === 'dark' ? styles.amountPickerDark : styles.amountPicker
+          }
+          dropdownOffset={
+            theme === 'dark' ? { top: 0, left: 0 } : { top: 8, left: 0 }
+          }
+          rippleInsets={
+            theme === 'dark'
+              ? { top: 0, bottom: 0, right: 0, left: 0 }
+              : { top: 8, bottom: 0, right: 0, left: 0 }
+          }
+          textColor={theme === 'dark' ? '#EBEBEB' : 'rgba(0,0,0, .87)'}
+          itemColor={theme === 'dark' ? '#CBCBCB' : 'rgba(0,0,0, .54)'}
+          selectedItemColor={theme === 'dark' ? '#EBEBEB' : 'rgba(0,0,0, .87)'}
+          baseColor={theme === 'dark' ? '#4285B9' : 'rgba(0,0,0, .38)'}
         />
       </View>
       <View>
@@ -74,7 +96,10 @@ const AmountStep = ({
           label="Description"
           value={invoice.description}
           onChange={setDescription}
-          inputStyle={styles.descInput}
+          placeholder="(Optional)"
+          inputStyle={
+            theme === 'dark' ? styles.descInputDark : styles.descInput
+          }
           multiline
         />
       </View>
@@ -121,6 +146,11 @@ const styles = StyleSheet.create({
     marginBottom: 0,
     height: 45,
   },
+  amountSelectDark: {
+    width: '35%',
+    marginTop: 0,
+    height: 45,
+  },
   amountSelectInput: {
     borderBottomColor: CSS.Colors.TRANSPARENT,
     elevation: 4,
@@ -130,6 +160,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: CSS.Colors.BACKGROUND_WHITE,
   },
+  amountSelectInputDark: {
+    paddingHorizontal: 15,
+    height: 45,
+    alignItems: 'center',
+    backgroundColor: CSS.Colors.TRANSPARENT,
+    fontFamily: 'Montserrat-700',
+    color: '#EBEBEB',
+    fontSize: 20,
+  },
   amountPicker: { borderRadius: 15 },
+  amountPickerDark: {
+    borderRadius: 0,
+    backgroundColor: '#212937',
+  },
   descInput: { height: 100, borderRadius: 15, textAlignVertical: 'top' },
+  descInputDark: { height: 100, borderRadius: 0, textAlignVertical: 'top' },
 })
