1c8a4cc4dd4accf6043fcd0603b42075e3a53124
Daniel Lugo <danlugo92@gmail.com>
2020-09-18 17:35:01 -0400

    new auth reducer

--- reducers/index.js
+++ reducers/index.js
@@ -1,6 +1,5 @@
 import * as Common from 'shock-common'
 
-import AuthReducer from './AuthReducer'
 import HistoryReducer from './HistoryReducer'
 import WalletReducer from './WalletReducer'
 import NodeReducer from './NodeReducer'
@@ -16,9 +15,10 @@ import SettingsReducer from './SettingsReducer'
 import paymentsV2s from './paymentV2s'
 import tips from './tips'
 import decodedInvoices from './decoded-invoices'
+import auth from './auth'
 
 const rootReducer = {
-  auth: AuthReducer,
+  auth,
   history: HistoryReducer,
   wallet: WalletReducer,
   node: NodeReducer,
