3941a955714c33a6e33a733cd2da101fc6aa14d6
Daniel Lugo <danlugo92@gmail.com>
2020-10-03 11:27:51 -0400

    formatting

--- app/services/seedServer.ts
+++ app/services/seedServer.ts
@@ -1,103 +1,105 @@
-import {generateRandomBytes} from './encryption'
-import FilePickerManager from 'react-native-file-picker';
+import { generateRandomBytes } from './encryption'
+import FilePickerManager from 'react-native-file-picker'
 import {
   FilePickerCancel,
   FilePickerError,
-  FilePickerFile
+  FilePickerFile,
 } from 'react-native-file-picker'
 
 export const enrollToken = async (
-  serviceUrl:string,
-  seedToken : string
-) :Promise<string> => {
-  const token : string = await generateRandomBytes(32)
-  const data : object = {
-    seed_token:seedToken,
-    wallet_token:token
+  serviceUrl: string,
+  seedToken: string,
+): Promise<string> => {
+  const token: string = await generateRandomBytes(32)
+  const data: object = {
+    seed_token: seedToken,
+    wallet_token: token,
   }
   const res = await fetch(`${serviceUrl}/api/enroll_token`, {
-    method: 'POST', 
+    method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data) 
+    body: JSON.stringify(data),
   })
-  if(res.ok){
+  if (res.ok) {
     return token
   }
-  throw new Error ("enroll token res NOT ok")
+  throw new Error('enroll token res NOT ok')
 }
 
-export const pickFile = () : Promise<FilePickerFile> =>{
-  return new Promise((res,rej)=>{
+export const pickFile = (): Promise<FilePickerFile> => {
+  return new Promise((res, rej) => {
     FilePickerManager.showFilePicker(response => {
       if ((response as FilePickerCancel).didCancel) {
-        rej('User cancelled file picker');
-      }
-      else if ((response as FilePickerError).error) {
-        rej('FilePickerManager Error: '+ (response as FilePickerError).error);
-      }
-      else {
+        rej('User cancelled file picker')
+      } else if ((response as FilePickerError).error) {
+        rej('FilePickerManager Error: ' + (response as FilePickerError).error)
+      } else {
         res(response as FilePickerFile)
       }
     })
   })
-  
 }
 export interface TorrentFile {
-  name:string
-  web_seed:string
-  hash:string
-  magnet:string
+  name: string
+  web_seed: string
+  hash: string
+  magnet: string
 }
 
 interface TorrentFileRes {
-  data :{
-    torrent:TorrentFile
+  data: {
+    torrent: TorrentFile
   }
 }
 /*WARNING FilePickerFile does not provide a field "name" but it must be provided to putFile,
   make sure to add it before passing the object file.name = file.fileName*/
 export const putFile = async (
   serviceUrl: string,
-  token : string,
-  files : FilePickerFile[], 
+  token: string,
+  files: FilePickerFile[],
   extraInfo: string = '',
-  comment:string = ''
-) : Promise<TorrentFile> =>  {
+  comment: string = '',
+): Promise<TorrentFile> => {
   const formData = new FormData()
-  files.forEach(file =>{
-    const fileComplete:(FilePickerFile & {name:string}) = {
+  files.forEach(file => {
+    const fileComplete: FilePickerFile & { name: string } = {
       ...file,
-      name:file.fileName
+      name: file.fileName,
     }
-    formData.append("files",fileComplete)
+    formData.append('files', fileComplete)
   })
-  formData.append("info",extraInfo)
-  formData.append("comment",comment)
-  const res = await fetch(`${serviceUrl}/api/put_file`,{
-    method: 'POST', 
-    headers:{
-      Authorization:`Bearer ${token}`,
+  formData.append('info', extraInfo)
+  formData.append('comment', comment)
+  const res = await fetch(`${serviceUrl}/api/put_file`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
     },
-    body: formData 
+    body: formData,
   })
-  if (res.ok){
-    const torrent = await res.json() as TorrentFileRes
+  if (res.ok) {
+    const torrent = (await res.json()) as TorrentFileRes
     return torrent.data.torrent as TorrentFile
   }
-  throw new Error ("put file res NOT ok")
+  throw new Error('put file res NOT ok')
 }
 
-const allowedTypes:string[] = ['image/jpeg','video/mpeg','video/mp4','image/png']
+const allowedTypes: string[] = [
+  'image/jpeg',
+  'video/mpeg',
+  'video/mp4',
+  'image/png',
+]
 
-export const isAllowedFormat = (type : string) :boolean => {
-
+export const isAllowedFormat = (type: string): boolean => {
   return allowedTypes.includes(type)
 }
 
-
-export const getMediaType = (type : string) :'image/embedded'|'video/embedded' =>{
-  if (type.includes(`image/`)) return "image/embedded"
+export const getMediaType = (
+  type: string,
+): 'image/embedded' | 'video/embedded' => {
+  if (type.includes(`image/`)) return 'image/embedded'
   return 'video/embedded'
 }
