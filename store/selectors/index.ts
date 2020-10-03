0892e69e1345f2383e9ac9fb2e98154e32a9e488
Daniel Lugo <danlugo92@gmail.com>
2020-10-03 00:46:46 -0400

    tx selectors

--- store/selectors/index.ts
+++ store/selectors/index.ts
@@ -3,6 +3,7 @@ import { State } from '../../reducers'
 export * from './connection'
 export * from './invoices'
 export * from './payments'
+export * from './tx'
 
 /**
  * For use inside sagas, allows typing of the return value from the select
