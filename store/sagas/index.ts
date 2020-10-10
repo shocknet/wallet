3e347bf9b342f2c4c2869453ca8f0640cf9e651a
Daniel Lugo <danlugo92@gmail.com>
2020-10-09 20:04:48 -0400

    chain txs saga

--- store/sagas/index.ts
+++ store/sagas/index.ts
@@ -3,9 +3,15 @@ import { all, call } from 'redux-saga/effects'
 
 import invoices from './invoices'
 import payments from './payments'
+import chainTXs from './chain-txs'
 
 function* rootSaga() {
-  yield all([call(Common.Store.rootSaga), call(invoices), call(payments)])
+  yield all([
+    call(Common.Store.rootSaga),
+    call(invoices),
+    call(payments),
+    call(chainTXs),
+  ])
 }
 
 export default rootSaga
