96a5a22e3d5bad91fa30b6b082c42247a5cd1da8
Daniel Lugo <danlugo92@gmail.com>
2020-10-13 16:02:17 -0400

    link cache service to redux store

--- app/services/cache.js
+++ app/services/cache.js
@@ -7,6 +7,8 @@ import Http from 'axios'
 import fromPairs from 'lodash/fromPairs'
 
 import { AUTH } from '../navigators/Root'
+import { getStore } from '../../store'
+import * as Actions from '../actions'
 
 import * as Navigation from './navigation'
 import * as Utils from './utils'
@@ -151,6 +153,9 @@ export const writeStoredAuthData = async authData => {
   if (authData === null) {
     Navigation.navigate(AUTH)
 
+    getStore().dispatch(Actions.tokenDidInvalidate())
+    getStore().dispatch(Actions.hostWasSet(''))
+
     return AsyncStorage.removeItem(STORED_AUTH_DATA).then(() => {
       authListeners.forEach(l => l())
     })
@@ -203,6 +208,15 @@ export const writeStoredAuthData = async authData => {
     authData,
     nodeIP: nodeURL.split(':')[0],
   }
+
+  getStore().dispatch(Actions.hostWasSet(nodeURL))
+  getStore().dispatch(
+    Actions.authed({
+      alias: authData.alias,
+      gunPublicKey: authData.publicKey,
+      token: authData.token,
+    }),
+  )
 
   await Promise.all([
     writeCachedAlias(authData.alias),
