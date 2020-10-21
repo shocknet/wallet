c174f32cb1fd3f9ec81b0952ff35e4fa777fd416
Daniel Lugo <danlugo92@gmail.com>
2020-10-20 20:50:19 -0400

    posts selectors

--- store/selectors/posts.ts
+++ store/selectors/posts.ts
@@ -1,3 +1,4 @@
+import { createSelector } from 'reselect'
 import { Schema } from 'shock-common'
 
 import { State } from '../../reducers'
@@ -6,3 +7,22 @@ export const getPosts = (state: State) => state.posts
 
 export const getPost = (state: State, id: string): Schema.PostN | null =>
   state.posts[id] || null
+
+const getPublicKey = (_: State, publicKey: string) => publicKey
+
+export const makeGetPostsForPublicKey = () =>
+  createSelector<
+    State,
+    string, // Props to selectors
+    ReturnType<typeof getPosts>,
+    ReturnType<typeof getPublicKey>,
+    Schema.PostN[] // Return type
+  >(
+    getPosts,
+    getPublicKey,
+    (posts, publicKey) => {
+      return Object.values(posts)
+        .filter(p => p.author === publicKey)
+        .sort((a, b) => b.date - a.date)
+    },
+  )
