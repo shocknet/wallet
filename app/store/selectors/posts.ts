8fe80961c38c4b00f9b538765792212df9db0d97
Daniel Lugo <danlugo92@gmail.com>
2020-10-28 17:58:01 -0400

    account for pinned posts in selector

--- app/store/selectors/posts.ts
+++ app/store/selectors/posts.ts
@@ -4,6 +4,7 @@ import { Schema } from 'shock-common'
 import { State } from '../reducers'
 
 import { getFollowedPublicKeys } from './follows'
+import { makeGetUser } from './users'
 
 export const getPosts = (state: State) => state.posts
 
@@ -12,22 +13,39 @@ export const getPost = (state: State, id: string): Schema.PostN | null =>
 
 const getPublicKey = (_: State, publicKey: string) => publicKey
 
-export const makeGetPostsForPublicKey = () =>
-  createSelector<
+export const makeGetPostsForPublicKey = () => {
+  const getUser = makeGetUser()
+
+  return createSelector<
     State,
     string, // Props to selectors
     ReturnType<typeof getPosts>,
     ReturnType<typeof getPublicKey>,
+    ReturnType<typeof getUser>,
     Schema.PostN[] // Return type
   >(
     getPosts,
     getPublicKey,
-    (posts, publicKey) => {
-      return Object.values(posts)
+    getUser,
+    (posts, publicKey, user) => {
+      const postsByThisPublicKey = Object.values(posts)
         .filter(p => p.author === publicKey)
         .sort((a, b) => b.date - a.date)
+
+      if (user.pinnedPost) {
+        const idx = postsByThisPublicKey.findIndex(
+          p => p.id === user.pinnedPost,
+        )
+        if (idx > 0) {
+          const [pinned] = postsByThisPublicKey.splice(idx, 1)
+          postsByThisPublicKey.splice(0, 0, pinned)
+        }
+      }
+
+      return postsByThisPublicKey
     },
   )
+}
 
 export const getPostsFromFollowed = createSelector<
   State,
