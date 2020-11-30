c2a42597928d9e20b9ca4b1aac2142ccfc7f4eff
Daniel Lugo <danlugo92@gmail.com>
2020-11-30 05:57:03 -0400

    migrate to class

--- app/screens/MyProfile/MetaConfigModal.tsx
+++ app/screens/MyProfile/MetaConfigModal.tsx
@@ -13,75 +13,84 @@ import Pad from '../../components/Pad'
 import InputGroup from '../../components/InputGroup'
 import ArrowLeft from '../../assets/images/arrow-left.svg'
 
-function MetaConfigModal(props: any) {
-  return (
-    <>
-      <StatusBar
-        translucent
-        backgroundColor="rgba(22, 25, 28, .94)"
-        barStyle="light-content"
-      />
+interface MetaConfigModalState {}
 
-      <View style={CSS.styles.flex}>
-        <Modal
-          isVisible={props.isModalVisible}
-          backdropColor="#16191C"
-          backdropOpacity={0.94}
-          animationIn="zoomInDown"
-          animationOut="zoomOutUp"
-          animationInTiming={600}
-          animationOutTiming={600}
-          backdropTransitionInTiming={600}
-          backdropTransitionOutTiming={600}
-          onBackdropPress={props.toggleModal}
-        >
-          <View style={styles.container}>
-            <View>
-              <View style={styles.backButton}>
-                <TouchableOpacity onPress={props.toggleModal}>
-                  <ArrowLeft size={19} />
-                </TouchableOpacity>
-              </View>
+export default class MetaConfigModal extends React.PureComponent<
+  any,
+  MetaConfigModalState
+> {
+  render() {
+    const { props } = this
+
+    return (
+      <>
+        <StatusBar
+          translucent
+          backgroundColor="rgba(22, 25, 28, .94)"
+          barStyle="light-content"
+        />
+
+        <View style={CSS.styles.flex}>
+          <Modal
+            isVisible={props.isModalVisible}
+            backdropColor="#16191C"
+            backdropOpacity={0.94}
+            animationIn="zoomInDown"
+            animationOut="zoomOutUp"
+            animationInTiming={600}
+            animationOutTiming={600}
+            backdropTransitionInTiming={600}
+            backdropTransitionOutTiming={600}
+            onBackdropPress={props.toggleModal}
+          >
+            <View style={styles.container}>
               <View>
-                <Text style={styles.caption}>Brand Config</Text>
+                <View style={styles.backButton}>
+                  <TouchableOpacity onPress={props.toggleModal}>
+                    <ArrowLeft size={19} />
+                  </TouchableOpacity>
+                </View>
+                <View>
+                  <Text style={styles.caption}>Brand Config</Text>
+                </View>
+                <Pad amount={40} />
+
+                <InputGroup
+                  label="Web Client"
+                  value=""
+                  isWebClientPicker
+                  publicKey={props.publicKey}
+                />
+
+                <InputGroup
+                  label="Webtorrent Seed"
+                  value=""
+                  placeholder="https://satoshi.watch/users_pubkey"
+                />
               </View>
-              <Pad amount={40} />
 
-              <InputGroup
-                label="Webtorrent Seed"
-                value=""
-                placeholder="https://satoshi.watch/users_pubkey"
-              />
-
-              <InputGroup
-                label="Web Client"
-                value=""
-                isWebClientPicker
-                publicKey={props.publicKey}
-              />
+              <View style={CSS.styles.rowCenteredSpaceBetween}>
+                <TouchableHighlight
+                  underlayColor="transparent"
+                  onPress={props.toggleModal}
+                  style={styles.actionButtonDark1}
+                >
+                  <Text style={styles.actionButtonTextDark1}>Cancel</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                  underlayColor="transparent"
+                  onPress={props.onPressSave}
+                  style={[styles.actionButtonDark2]}
+                >
+                  <Text style={styles.actionButtonTextDark2}>Save</Text>
+                </TouchableHighlight>
+              </View>
             </View>
-
-            <View style={CSS.styles.rowCenteredSpaceBetween}>
-              <TouchableHighlight
-                underlayColor="transparent"
-                onPress={props.onPressCancel}
-                style={styles.actionButtonDark1}
-              >
-                <Text style={styles.actionButtonTextDark1}>Cancel</Text>
-              </TouchableHighlight>
-              <TouchableHighlight
-                underlayColor="transparent"
-                onPress={props.onPressSave}
-                style={[styles.actionButtonDark2]}
-              >
-                <Text style={styles.actionButtonTextDark2}>Save</Text>
-              </TouchableHighlight>
-            </View>
-          </View>
-        </Modal>
-      </View>
-    </>
-  )
+          </Modal>
+        </View>
+      </>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -150,7 +159,3 @@ const styles = StyleSheet.create({
     fontSize: 14,
   },
 })
-
-const MemoizedMetaConfigModal = React.memo(MetaConfigModal)
-
-export default MemoizedMetaConfigModal
